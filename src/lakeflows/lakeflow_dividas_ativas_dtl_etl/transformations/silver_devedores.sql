709b57ad465cd9348085eb7ed050dc8c58da1c79
xjhowxjhow <jhow.1516@hotmail.com>
2025-11-26 02:14:30 +0000

    up

--- src/lakeflows/lakeflow_dividas_ativas_dtl_etl/transformations/silver_devedores.sql
+++ src/lakeflows/lakeflow_dividas_ativas_dtl_etl/transformations/silver_devedores.sql
@@ -4,38 +4,16 @@ TBLPROPERTIES ('quality' = 'silver')
 AS
 SELECT
   regexp_replace(cpf_cnpj_devedor, '[^0-9Xx]', '') AS cpf_cnpj,
-  upper(translate(regexp_replace(nome_devedor, '\uFFFD', ''),
-    'ÁÂÀÃÄÅáâàãäåÉÊÈËéêèëÍÎÌÏíîìïÓÔÒÕÖóôòõöÚÛÙÜúûùüÇçÑñ',
-    'AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuCcNn'
-  )) AS nome_devedor,
-  upper(translate(regexp_replace(tipo_pessoa, '\uFFFD', ''),
-    'ÁÂÀÃÄÅáâàãäåÉÊÈËéêèëÍÎÌÏíîìïÓÔÒÕÖóôòõöÚÛÙÜúûùüÇçÑñ',
-    'AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuCcNn'
-  )) AS tipo_pessoa,
-  upper(translate(regexp_replace(tipo_devedor, '\uFFFD', ''),
-    'ÁÂÀÃÄÅáâàãäåÉÊÈËéêèëÍÎÌÏíîìïÓÔÒÕÖóôòõöÚÛÙÜúûùüÇçÑñ',
-    'AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuCcNn'
-  )) AS tipo_devedor,
-  upper(translate(regexp_replace(origem, '\uFFFD', ''),
-    'ÁÂÀÃÄÅáâàãäåÉÊÈËéêèëÍÎÌÏíîìïÓÔÒÕÖóôòõöÚÛÙÜúûùüÇçÑñ',
-    'AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuCcNn'
-  )) AS origem,
-  upper(translate(regexp_replace(credor, '\uFFFD', ''),
-    'ÁÂÀÃÄÅáâàãäåÉÊÈËéêèëÍÎÌÏíîìïÓÔÒÕÖóôòõöÚÛÙÜúûùüÇçÑñ',
-    'AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuCcNn'
-  )) AS credor,
+  dev.dividas.remove_acentos(nome_devedor) AS nome_devedor,
+  dev.dividas.remove_acentos(tipo_pessoa) AS tipo_pessoa,
+  dev.dividas.remove_acentos(tipo_devedor) AS tipo_devedor,
+  dev.dividas.remove_acentos(origem) AS origem,
+  dev.dividas.remove_acentos(credor) AS credor,
   CAST(regexp_replace(numero_termo_inscricao, '[^0-9]', '') AS BIGINT) AS termo,
   TO_DATE(data_da_inscricao, 'dd/MM/yyyy') AS data_inscricao,
   sequencial_credito,
-  upper(translate(regexp_replace(situacao_credito, '\uFFFD', ''),
-    'ÁÂÀÃÄÅáâàãäåÉÊÈËéêèëÍÎÌÏíîìïÓÔÒÕÖóôòõöÚÛÙÜúûùüÇçÑñ',
-    'AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuCcNn'
-  )) AS situacao_credito,
-
-  upper(translate(regexp_replace(motivo_suspensao, '\uFFFD', ''),
-    'ÁÂÀÃÄÅáâàãäåÉÊÈËéêèëÍÎÌÏíîìïÓÔÒÕÖóôòõöÚÛÙÜúûùüÇçÑñ',
-    'AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUUuuuuCcNn'
-  )) AS motivo_suspensao,
+  dev.dividas.remove_acentos(situacao_credito) AS situacao_credito,
+  dev.dividas.remove_acentos(motivo_suspensao) AS motivo_suspensao,
   ano,
   mes,
   CAST(
@@ -44,10 +22,10 @@ SELECT
         regexp_replace(saldo_devedor_sem_honorarios, '[^0-9,.-]', ''), 
         ',', '.'
       ),
-      '\\.(?=.*\\.)', ''  -- remove pontos de milhar, deixa só o último
+      '\\.(?=.*\\.)', ''
     ) AS DOUBLE
   ) AS saldo_devedor,
   CURRENT_TIMESTAMP() AS data_processamento
-  
+
 FROM STREAM dev.dividas.devedores_bronze
 WHERE cpf_cnpj_devedor IS NOT NULL;
